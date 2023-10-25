6efa48fcc39a6af75e26bc3c5ce7ed2c89ad3e0a
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-10-25 14:37:38 +0200

    cosas

--- Ej241/arrayParametrosVariables.php
+++ Ej241/arrayParametrosVariables.php
@@ -3,23 +3,21 @@
 Una función que devuelva el mayor de todos los números recibidos como parámetros: function mayor(): int. Utiliza las funciones func_get_args(), etc... No puedes usar la función max().
 Una función que concatene todos los parámetros recibidos separándolos con un espacio: function concatenar(...$palabras) : string. Utiliza el operador ....
 -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parámetros Variables</title>
-</head>
-<body>
-    <?php
-        function mayor() : int {
-            if (func_get_args() == 0 ) {
-                return 0;
-            } else {
-                $mayor = 0;
-                $menor = PHP_INT_MAX;
-            }
+<?php
+function mayor(): int {
+    $mayor = 0;
+    $parametros = func_get_args();
+    foreach ($parametros as $num) {
+        if ($num > $mayor) {
+            $mayor = $num;
         }
-    ?>
-</body>
-</html>
+    }
+    return $mayor;
+}
+
+echo "El numero mayor de los siguientes es: " . mayor(5, 6, 3, 9, 17, 745, 1);
+
+
+
+
+?>
