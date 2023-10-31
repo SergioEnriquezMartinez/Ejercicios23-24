6eb586617b27cdd84b63c1f29b5ddcd454d4319a
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-10-31 14:37:31 +0100

    un poco del 302

--- Ej302/empleadoTelefonos.php
+++ Ej302/empleadoTelefonos.php
@@ -3,5 +3,54 @@
 public function anyadirTelefono(int $telefono) : void → Añade un teléfono al array
 public function listarTelefonos(): string → Muestra los teléfonos separados por comas*/
 
+    class Empleado {
+
+        public function __construct(
+            private string $nombre,
+            private string $apellido,
+            private int $sueldo,
+            private int $telefonos = []         
+        ){}
+
+
+        public function setNombre(string $nom) {
+            $this->nombre = $nom;
+        }
+        public function setApellido(string $ape) {
+            $this->apellido = $ape;
+        }
+        public function setSueldo(int $s) {
+            $this->sueldo = $s;
+        }
+
+        public function getNombre() : string {
+            return $this->nombre;
+        }
+        public function getApellido() : string {
+            return $this->apellido;
+        }
+        public function getSueldo() : int {
+            return $this->sueldo;
+        }
+
+
+        public function getNombreCompleto() : string {
+            return $this->nombre . " " . $this->apellido;
+        }
+
+        public function debePagarImpuestos() : bool {
+            return $this->sueldo > 3333;
+        }
+
+        public function aniadirTelefono(int $telefono) : void {
+            array_push($telefonos, $telefono);
+        }
+
+        public function listarTelefonos() : string {
+            return "";
+        }
+    }
+
+
 
 ?>
