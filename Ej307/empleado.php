765e9187391e34aa450f1b391a3a84274c4216da
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-11-08 22:36:02 +0100

    cosas

--- Ej307/empleado.php
+++ Ej307/empleado.php
@@ -1,62 +1,32 @@
 <?php
-class Empleado307 {
+class Empleado307 extends Persona307 {
 
-public function __construct(
-    private string $nombre,
-    private string $apellido,
-    private int $sueldo,
-    private int $telefonos = []         
-){}
+    private int $sueldo;
 
 
-public function setNombre(string $nom) {
-    $this->nombre = $nom;
-}
-public function setApellido(string $ape) {
-    $this->apellido = $ape;
-}
-public function setSueldo(int $s) {
-    $this->sueldo = $s;
-}
-
-public function getNombre() : string {
-    return $this->nombre;
-}
-public function getApellido() : string {
-    return $this->apellido;
-}
-public function getSueldo() : int {
-    return $this->sueldo;
-}
-public function getTelefono() : int {
-    foreach ($this->telefonos as $telf) {
-        return $telf;
+    public function __construct(string $nombre, string $apellidos, int $sueldo){
+        parent::__construct($nombre, $apellidos);
+        $this->sueldo = $sueldo;
     }
-}
 
 
-public function getNombreCompleto() : string {
-    return $this->nombre . " " . $this->apellido;
-}
-
-public function debePagarImpuestos() : bool {
-    return $this->sueldo > 3333;
-}
-
-public function aniadirTelefono(int $telefono) : void {
-    array_push($this->telefonos, $telefono);
-}
-
-public function listarTelefonos() : string {
-    foreach ($this->telefonos as $telfs) {
-        return $telfs;
+    public function setSueldo(int $s) {
+        $this->sueldo = $s;
     }
-}
 
-public static function toHtml($emp) : string {
-    return "<p>Empleado: " . $emp->getNombreCompleto() . " con un sueldo de " . $emp->getSueldo(). " y con los teléfonos " . $emp->getTelefono() . "</p>";
-}
+    public function getSueldo() : int {
+        return $this->sueldo;
+    }
+
+
+
+    public function debePagarImpuestos() : bool {
+        return $this->sueldo > 3333;
+    }
+
+    public static function toHtml($emp) : string {
+        return "<p>Empleado: " . $emp->getNombreCompleto() . " con un sueldo de " . $emp->getSueldo . "</p>";
+    }
 
 
 }
-?>
--- Ej307/persona.php
+++ Ej307/persona.php
@@ -1,3 +1,31 @@
 <?php
 /*Copia la clase del ejercicio anterior en 307Empleado.php y modifícala.
 Crea una clase Persona que sea padre de Empleado, de manera que Persona contenga el nombre y los apellidos, y en Empleado quede el salario y los teléfonos.*/
+
+class Persona307 {
+    private string $nombre;
+    private string $apellido;
+
+    public function __construct(string $nombre, string $apellido){
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+    }
+
+    public function setNombre(string $nom) {
+        $this->nombre = $nom;
+    }
+    public function setApellido(string $ape) {
+        $this->apellido = $ape;
+    }
+
+    public function getNombre() : string {
+        return $this->nombre;
+    }
+    public function getApellido() : string {
+        return $this->apellido;
+    }
+    
+    public function getNombreCompleto() : string {
+        return $this->nombre . " " . $this->apellido;
+    }
+}
