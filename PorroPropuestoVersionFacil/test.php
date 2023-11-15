b7c72a88493a093891f08ee040f018ca243d95ce
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-11-15 19:41:57 +0100

    hola michel

--- PorroPropuestoVersionFacil/test.php
+++ PorroPropuestoVersionFacil/test.php
@@ -10,14 +10,16 @@
         $nombre = $_GET["nombre"];
         $sumas = $_GET["sumas"];
         $restas = $_GET["restas"];
-        $sumaTotal = $_GET["sumaTotal"];
-        $restaTotal = $_GET["restaTotal"];
+        $resultadoSuma = intval($_GET["resultadoSuma"]);
+        $resultadoResta = intval($_GET["resultadoResta"]);
+        $resultadoEsperadoAnterior = intval($_GET["resultadoEsperado"]);
+        $i = intval($_GET["i"]);
 
-        if (empty($nombre) && !preg_match("/^[a-zA-Z ]*$/", $nombre)) {
+        if (empty($nombre) || !preg_match("/^[a-zA-Z ]*$/", $nombre)) {
             echo "<p>Introduce un nombre válido</p>";
         }
         
-        if (is_numeric($numSumas) && is_numeric($numRestas)) {
+        if (is_numeric($sumas) && is_numeric($restas)) {
             $numSumas = intval($sumas);
             $numRestas = intval($restas);
             if ($numSumas >= 0 && $numRestas >= 0) {
@@ -29,33 +31,41 @@
             echo "<p>Introduce valores numéricos</p>";
         }
 
+        if ($resultadoSuma == $resultadoEsperadoAnterior) {
+            $sumasCorrectas++;
+        } else if ($resultadoResta == $resultadoEsperadoAnterior) {
+            $restasCorrectas++;
+        }
+
         echo "<h3>Test $i/$numOperaciones</h3>";
         $sumaOresta = rand(0,1) > 0.5;
 
-        $operando1 = rand(10, 99);
-        $operando2 = rand(10, 99);
+        $operando1 = intval(rand(10, 99));
+        $operando2 = intval(rand(10, 99));
         if ($sumaOresta && $contSumas <= $numSumas) {
             $resultadoEsperado = $operando1 + $operando2;
-            echo "<h2>¿Cuánto suman $operando1 + $operando2?</h2>";
-            echo "<form>";
-                echo "<input type=\"number\" name=\sumaTotal\" id=\"sumaTotal\">";
-                echo "<input type=\"hidden\" value=\"$resultadoEsperado\">";
-                echo "<input type=\"hidden\" value=\"$i\">";
-                echo "<input type=\"hidden\" value=\"$numOperaciones\">";
-            echo "</form>";
             $contSumas++;
             $i++;
+            echo "<h2>¿Cuánto suman $operando1 + $operando2?</h2>";
+            echo "<formn action=\"test.php\" method=\"get\">";
+                echo "<input type=\"number\" name=\"resultadoSuma\" id=\"resultadoSuma\">";
+                echo "<input type=\"hidden\" name=\"resultadoEsperado\" value=\"$resultadoEsperado\">";
+                echo "<input type=\"hidden\" name=\"i\" value=\"$i\">";
+                echo "<input type=\"hidden\" name=\"numOperaciones\" value=\"$numOperaciones\">";
+                echo "<input type=\"hidden\" name=\"contSumas\" value=\"$contSumas\">";
+            echo "</form>";
         } else if (!$sumaOresta && $contRestas <= $numRestas) {
             $resultadoEsperado = $operando1 - $operando2;
-            echo "<h2>¿Cual es el resultado de $operando1 - $operando2?</h2>";
-            echo "<form>";
-                echo "<input type=\"number\" name=\"restaTotal\" id=\"restaTotal\">";
-                echo "<input type=\"hidden\" value=\"$resultadoEsperado\">";
-                echo "<input type=\"hidden\" value=\"$i\">";
-                echo "<input type=\"hidden\" value=\"$numOperaciones\">";
-            echo "</form>";
             $contRestas++;
             $i++;
+            echo "<h2>¿Cual es el resultado de $operando1 - $operando2?</h2>";
+            echo "<form action=\"test.php\" method=\"get\">";
+                echo "<input type=\"number\" name=\"resultadoResta\" id=\"resultadoResta\">";
+                echo "<input type=\"hidden\" name=\"resultadoEsperado\" value=\"$resultadoEsperado\">";
+                echo "<input type=\"hidden\" name=\"i\" value=\"$i\">";
+                echo "<input type=\"hidden\" name=\"numOperaciones\" value=\"$numOperaciones\">";
+                echo "<input type=\"hidden\" name=\"contRestas\" value=\"$contRestas\">";
+            echo "</form>";
         }
         
     ?>
