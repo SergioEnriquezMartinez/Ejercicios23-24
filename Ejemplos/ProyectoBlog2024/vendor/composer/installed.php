4ac74bebe87def4a144bb7450e3d8837d8bd4f3e
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2024-01-31 11:32:37 +0100

    xdxdddd

--- Ejemplos/ProyectoBlog2024/vendor/composer/installed.php
+++ Ejemplos/ProyectoBlog2024/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'sergi/proyecto-blog2024',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c8ff7c130a73c84b6941391f8fb3e5223acf4d82',
+        'reference' => 'c671d025c0cc9214800d7e2d63ab4b4ee92fa677',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'sergi/proyecto-blog2024' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c8ff7c130a73c84b6941391f8fb3e5223acf4d82',
+            'reference' => 'c671d025c0cc9214800d7e2d63ab4b4ee92fa677',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
